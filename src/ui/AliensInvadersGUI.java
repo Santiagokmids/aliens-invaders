6e124c6d1bfa34f93aade080f61682995b478fc8
Santiago <sntgtrochez@gmail.com>
2021-05-24 14:53:00 -0500

    Package images already

--- src/ui/AliensInvadersGUI.java
+++ src/ui/AliensInvadersGUI.java
@@ -1,5 +1,15 @@
 package ui;
 
-public class AliensInvadersGUI {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class AliensInvadersGUI {
+	
+	@FXML
+    private Button btnStart;
+
+    @FXML
+    private Button btnScores;
+    
+    
 }
