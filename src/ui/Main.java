710df520cf147e8c16a54da5a55727ea4df45d64
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-05-25 17:30:53 -0500

    Some changes

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-pain.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-pane.fxml"));
 		
 		fxmlLoader.setController(aliendsInvadersGUI);
 		
