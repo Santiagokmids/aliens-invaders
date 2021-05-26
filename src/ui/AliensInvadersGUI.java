6ee6a3b03f3d870b935b3f4d8db820342e15411d
Santiago <sntgtrochez@gmail.com>
2021-05-25 19:30:01 -0500

    All scenes and buttons already

--- src/ui/AliensInvadersGUI.java
+++ src/ui/AliensInvadersGUI.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -33,6 +34,18 @@ public class AliensInvadersGUI {
     
     @FXML
     private ImageView imageBackGround;
+    
+    @FXML
+    private ImageView backGround;
+
+    @FXML
+    private ImageView gameOver;
+
+    @FXML
+    private ImageView insertName;
+
+    @FXML
+    private TextField nickName;
 
 	public AliensInvadersGUI() {
 	}
@@ -56,7 +69,7 @@ public class AliensInvadersGUI {
 	}
 	
 	@FXML
-    void btnStartGame(ActionEvent event) throws IOException {
+    public void btnStartGame(ActionEvent event) throws IOException {
     	
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("game-pain.fxml"));
     	
@@ -72,7 +85,7 @@ public class AliensInvadersGUI {
     }
     
     @FXML
-    void btnScores(ActionEvent event) throws IOException {
+    public void btnScores(ActionEvent event) throws IOException {
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("hallOfFame.fxml"));
 
 		loader.setController(this);
@@ -82,6 +95,25 @@ public class AliensInvadersGUI {
 		paneHall.setImage(image);
 		Image image2 = new Image("/images/halll.png");
 		lblHall.setImage(image2);
+		
+		mainPane.getChildren().clear();
+		mainPane.setTop(load);
+
+    }
+    
+    @FXML
+    public void acept(ActionEvent event) throws IOException {
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("gameover-pain.fxml"));
+
+		loader.setController(this);
+		Parent load = loader.load();
+		
+		Image image = new Image("/images/insertName.png");
+		insertName.setImage(image);
+		Image image2 = new Image("/images/gameOver.png");
+		gameOver.setImage(image2);
+		Image image3 = new Image("/images/backGround.png");
+		backGround.setImage(image3);
 		
 		mainPane.getChildren().clear();
 		mainPane.setTop(load);
