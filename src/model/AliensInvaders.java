4e4de0a4226d9d2485f62a2108b53404694ebc85
Santiago <sntgtrochez@gmail.com>
2021-05-28 08:38:39 -0500

    Desing of test already

--- src/model/AliensInvaders.java
+++ src/model/AliensInvaders.java
@@ -1,6 +1,39 @@
 package model;
 
 public class AliensInvaders {
+
+	public AliensInvaders() {
+	}
 	
+	public Player searchPlayers(String name) {
+		return null;
+	}
 	
+	public boolean addPeople(String name) {
+		return true;
+	}
+	
+	public boolean addPlayer(String nick, int score, int level) {
+		return true;
+	}
+	
+	public Player searchScore(int score) {
+		return null;
+	}
+	
+	public boolean removePlayer(String name, int score) {
+		return true;
+	}
+	
+	public int calculateScore(int score, int level) {
+		return 0;
+	}
+	
+	public boolean verifyName(String name) {
+		return true;
+	}
+	
+	public boolean verifyScores(int score) {
+		return true;
+	}
 }
